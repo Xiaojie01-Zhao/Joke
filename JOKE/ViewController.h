2b15de09055362ff91847e4808b93bf3c1c81256
Zhao <763764553@qq.com>
2015-07-25 15:25:55 +0800

    Initial Commit

---
+++ JOKE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JOKE
+//
+//  Created by lanouhn on 15/7/25.
+//  Copyright (c) 2015年 ACE--赵肖杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JOKE/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JOKE
+//
+//  Created by lanouhn on 15/7/25.
+//  Copyright (c) 2015年 ACE--赵肖杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
